228f7bfc1bdadb1ba277aaee43405937bfe7a2d2
BenOAnsah <benoansah99@gmail.com>
2026-09-17 12:03:54 -0400

    Task 6 cleared, display breakthrough pending

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
 import avReducer from './avSlice'
+import mealsR from './mealsSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
-    av: av
+    av: avReducer,
+    meals: mealsReducer
   },
 });
